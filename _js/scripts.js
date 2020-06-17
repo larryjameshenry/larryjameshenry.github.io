0d07a9230b9c6373a2d2d128c376a7862a261532
Larry Henry <Larry.Henry@cbn.org>
2020-06-17 16:23:39 -0400

    updates

--- _js/scripts.js
+++ _js/scripts.js
@@ -2,7 +2,6 @@ window.jQuery = window.$ = require( "jquery" );
 require( "velocity-animate/velocity.js" );
 require( "lazysizes" );
 require( "lazysizes/plugins/unveilhooks/ls.unveilhooks.js" );
-
 // Jquery & Velocity JS included in GULP
 $( document ).ready( function() {
     toggleMobileNav();
@@ -10,12 +9,10 @@ $( document ).ready( function() {
     formCheck();
 
 } );
-
 // Close modal if ESC is pressed
 $( document ).keyup( function( e ) {
     e.keyCode === 27 ? removeModal() : null;
 } );
-
 $( window ).resize( function() {
     $( ".header" ).removeClass( "hide-nav" ); // Ensure nav will be shown on resize
     $( ".header__toggle" ).removeClass( "--open" );
@@ -23,11 +20,9 @@ $( window ).resize( function() {
     $( ".header__links" ).removeAttr( "style" ); // If mobile nav was collapsed, make sure it's show on DESK
     $( ".header__overlay" ).remove(); // Remove mobile navigation overlay in case it was opened
 } );
-
 /*-------------------------------------------------------------------------*/
 /* MOBILE NAVIGATION */
 /* -----------------------------------------------------------------------*/
-
 function toggleMobileNav() {
     $( ".header__toggle" ).click( function() {
       if ( !$( ".header__links" ).is( ".velocity-animating" ) ) {
@@ -46,7 +41,6 @@ function toggleMobileNav() {
       }
     } );
 }
-
 function openMobileNav() {
     $( ".header__links" ).velocity( "slideDown", {
         duration: 300,
@@ -65,7 +59,6 @@ function openMobileNav() {
         }
     } );
 }
-
 function hideMobileNav() {
     $( ".header__overlay" ).remove();
     $( ".header__links" ).velocity( "slideUp", {
@@ -85,46 +78,37 @@ function hideMobileNav() {
         }
     } );
 }
-
 /*-------------------------------------------------------------------------*/
 /* SHOW/SCROLL NAVIGATION */
 /* -----------------------------------------------------------------------*/
-
 function ShowHideNav() {
     var previousScroll = 0, // previous scroll position
         $header = $( ".header" ), // just storing header in a variable
         navHeight = $header.outerHeight(), // nav height
         detachPoint = 576 + 60, // after scroll past this nav will be hidden
         hideShowOffset = 6; // scroll value after which nav will be shown/hidden
-
     $( window ).scroll( function() {
         var wW = 1024;
-
         // if window width is more than 1024px start show/hide nav
         if ( $( window ).width() >= wW ) {
             if ( !$header.hasClass( "fixed" ) ) {
                 var currentScroll = $( this ).scrollTop(),
                     scrollDifference = Math.abs( currentScroll - previousScroll );
-
                 // if scrolled past nav
                 if ( currentScroll > navHeight ) {
-
                     // if scrolled past detach point -> show nav
                     if ( currentScroll > detachPoint ) {
                         if ( !$header.hasClass( "fix-nav" ) ) {
                             $header.addClass( "fix-nav" );
                         }
                     }
-
                     if ( scrollDifference >= hideShowOffset ) {
                         if ( currentScroll > previousScroll ) {
-
                             // scroll down -> hide nav
                             if ( !$header.hasClass( "hide-nav" ) ) {
                                 $header.addClass( "hide-nav" );
                               }
                         } else {
-
                             // scroll up -> show nav
                             if ( $header.hasClass( "hide-nav" ) ) {
                                 $( $header ).removeClass( "hide-nav" );
@@ -132,7 +116,6 @@ function ShowHideNav() {
                         }
                     }
                 } else {
-
                     // at the top
                     if ( currentScroll <= 0 ) {
                         $header.removeClass( "hide-nav show-nav" );
@@ -140,7 +123,6 @@ function ShowHideNav() {
                     }
                 }
             }
-
             // scrolled to the bottom -> show nav
             if ( ( window.innerHeight + window.scrollY ) >= document.body.offsetHeight ) {
                 $header.removeClass( "hide-nav" );
@@ -151,18 +133,15 @@ function ShowHideNav() {
         }
     } );
 }
-
 /*-------------------------------------------------------------------------*/
 /* HANDLE MODAL */
 /* -----------------------------------------------------------------------*/
-
 function openModal() {
     $( "body" ).css( "overflow", "hidden" );
     $( ".modal, .modal__overlay" ).show().css( "display", "flex" );
     $( ".modal__inner" ).velocity( { translateY: 0, opacity: 1 } );
     $( ".modal__overlay" ).velocity( { opacity: 1 }, 100 );
 }
-
 function removeModal() {
     $( "body" ).css( { "overflow": "visible" } );
     $( ".modal, .modal__overlay, .modal__inner" ).velocity( { opacity: 0 }, function() {
@@ -176,37 +155,27 @@ function removeModal() {
         $( ".modal__body" ).empty();
     } );
 }
-
 $( ".js-modal-close" ).click( function() {
     removeModal();
 } );
-
 $( ".modal__overlay" ).click( function() {
     removeModal();
 } );
-
 /*-------------------------------------------------------------------------*/
 /* FORM VALIDATION */
 /* -----------------------------------------------------------------------*/
-
 function formCheck() {
     $( ".js-submit" ).click( function( e ) {
-
         e.preventDefault();
-
         var $inputs = $( ".form__input input" );
         var textarea = $( ".form__input textarea" );
         var isError = false;
-
         $( ".form__input" ).removeClass( "error" );
         $( ".error-data" ).remove();
-
         for ( var i = 0; i < $inputs.length; i++ ) {
             var input = $inputs[ i ];
             if ( $( input ).attr( "required", true ) && !validateRequired( $( input ).val() ) ) {
-
                 addErrorData( $( input ), "This field is required" );
-
                 isError = true;
             }
             if ( $( input ).attr( "required", true ) && $( input ).attr( "type" ) === "email" && !validateEmail( $( input ).val() ) ) {
@@ -223,7 +192,6 @@ function formCheck() {
         }
     } );
 }
-
 // Validate if the input is not empty
 function validateRequired( value ) {
     if ( value === "" ) {
@@ -231,7 +199,6 @@ function validateRequired( value ) {
     }
     return true;
 }
-
 // Validate if the email is using correct format
 function validateEmail( value ) {
     if ( value !== "" ) {
@@ -239,62 +206,8 @@ function validateEmail( value ) {
     }
     return true;
 }
-
 // Add error message to the input
 function addErrorData( element, error ) {
     element.parent().addClass( "error" );
     element.after( "<span class='error-data'>" + error + "</span>" );
 }
-
-
-/*-------------------------------------------------------------------------*/
-/* AJAX FORM SUBMIT
-/* Formspree now only supports AJAX for Gold Users
-/* https://github.com/formspree/formspree/pull/173
-/* Uncomment if you want to use AJAX Form submission and you're a gold user
-/* -----------------------------------------------------------------------*/
-
-// $( "#contactForm" ).submit( function( e ) {
-
-//     e.preventDefault();
-
-//     var $btn = $( ".js-submit" ),
-//         $inputs = $( ".form__input input" ),
-//         $textarea = $( ".form__input textarea" ),
-//         $name = $( "input#name" ).val(),
-//         $url = $( "#contactForm" ).attr( "action" );
-
-//     $.ajax( {
-
-//         url: $url,
-//         method: "POST",
-//         data: $( this ).serialize(),
-//         dataType: "json",
-
-//         beforeSend: function() {
-//             $btn.prop( "disabled", true );
-//             $btn.text( "Sending..." );
-//         },
-//         // eslint-disable-next-line no-unused-vars
-//         success: function( data ) {
-//             $inputs.val( "" );
-//             $textarea.val( "" );
-//             $btn.prop( "disabled", false );
-//             $btn.text( "Send" );
-//             openModal();
-//             $( ".modal__body" ).append(
-//               "<h1>Thanks " +
-//               $name +
-//               "!</h1><p>Your message was successfully sent! Will get back to you soon.</p>"
-//             );
-
-//         },
-//         error: function( err ) {
-//             $( ".modal, .modal__overlay" ).addClass( "--show" );
-//             $( ".modal__body" ).append(
-//               "<h1>Aww snap!</h1><p>Something went wrong, please try again. Error message: </p>" +
-//               err
-//             );
-//         }
-//     } );
-// } );
